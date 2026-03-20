e3cf986bf6ce40e51653b1bb51d7524cf8d67fbc
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-03-20 18:40:22 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+// hereliesaz/reup/ReUp-c714b8692ef249c9d91ed57a33a63f43f5c8c59d/settings.gradle.kts
+
 /**
  * The epistemological foundation of the project.
  * Dictates where the machine is allowed to search for its required truths.
@@ -9,6 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    // Teaches the machine how to dynamically resolve toolchain requests 
+    // instead of choking on dead hardcoded links.
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
