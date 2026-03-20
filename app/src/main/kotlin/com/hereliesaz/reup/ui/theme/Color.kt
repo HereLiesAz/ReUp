90139d0e1a753e299e623339db7149b7d825471b
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-03-20 14:51:57 -0500

    Create Color.kt

---
+++ app/src/main/kotlin/com/hereliesaz/reup/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.hereliesaz.reup.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val VoidBlack = Color(0xFF000000)
+val HemorrhageRed = Color(0xFF8B0000)
+val TranslucentHemorrhage = Color(0x668B0000)
+val ConcreteGray = Color(0xFF1A1A1A)
+val AshGray = Color(0xFF808080)
